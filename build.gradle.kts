6fcc459f68ca43a45e0f32aa54957c08172d00af
Ivan Shilin <ivanshilin-shilin@mail.ru>
2024-05-12 18:23:30 +0300

    Последние изменения по конфигурации проекта

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,10 @@ group = "ru.ivan_shilin"
 version = "0.0.1"
 
 dependencies {
+    implementation(platform(libs.kotlinx.coroutines.bom))
     implementation(libs.bundles.coroutines)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
 }
 
 kotlin {
